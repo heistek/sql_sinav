b0ead8072f4240e8cb07e72765925019e4620c1c
heistek <116315640+heistek@users.noreply.github.com>
2023-06-22 14:52:48 +0300

    sql sinavi

---
+++ sql_sinavi.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Notlar ( OgrenciNo INT, DersKodu INT, SinavAdi VARCHAR(50), SinavPuani INT, HarfNotu VARCHAR(50) );
+
+INSERT INTO Notlar (OgrenciNo) VALUES (1), (2), (3);
+
+INSERT INTO Notlar (DersKodu) VALUES (101), (102), (101);
+
+INSERT INTO Notlar (SinavAdi) VALUES (Vize), (Final), (Final);
+
+INSERT INTO Notlar (SinavPuani) VALUES (85), (72), (65);
+
+INSERT INTO Notlar (HarfNotu) VALUES (AA), (BB), (CC);
